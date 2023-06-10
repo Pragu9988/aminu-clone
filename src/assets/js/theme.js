3a576ea341e02a09f07b1d3fb1a1b0587eb44a03
pragyan.chaudhary <chaudharypragyan@gmail.com>
2023-06-10 21:46:35 +0545

    Header component created

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -1,1 +1,29 @@
-console.log("work");
+// Get the announcement elements
+const announcements = document.querySelectorAll(".announcement");
+
+// Initialize index and timer variables
+let currentAnnouncementIndex = 0;
+let timer;
+
+// Function to display the next announcement
+function displayNextAnnouncement() {
+  // Hide the current announcement
+  announcements[currentAnnouncementIndex].classList.add(
+    "announcement--inactive"
+  );
+
+  // Increment the index for the next announcement
+  currentAnnouncementIndex =
+    (currentAnnouncementIndex + 1) % announcements.length;
+
+  // Show the next announcement
+  announcements[currentAnnouncementIndex].classList.remove(
+    "announcement--inactive"
+  );
+
+  // Start the timer for the next announcement
+  timer = setTimeout(displayNextAnnouncement, 5000);
+}
+
+// Start the timer for the first announcement
+timer = setTimeout(displayNextAnnouncement, 5000);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/html/**.html","./src/partials/**/*.html", "./src/js/**.js"],
-  darkMode: true,
+  content: ["./src/html/**.html", "./src/partials/**/*.html", "./src/js/**.js"],
+  darkMode: false,
+  prefix: "al-",
   theme: {
     extend: {},
   },

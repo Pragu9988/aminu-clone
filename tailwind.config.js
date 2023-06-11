f9c6eaf0ed6a2a2eb8e97ce3209763d49bb1a003
pragyan.chaudhary <chaudharypragyan@gmail.com>
2023-06-11 22:20:52 +0545

    gill sans font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/html/**.html", "./src/partials/**/*.html", "./src/js/**.js"],
-  darkMode: false,
   prefix: "al-",
   theme: {
     extend: {},
